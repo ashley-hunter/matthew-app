82446d2a91eb84452def2bebeff1346d0a659da1
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2025-07-03 21:02:46 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import BrakePressCalculator from './pages/BrakePressCalculator';
 
 // Root component for the Brake Press Costing Calculator application
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         {/* Main route rendering the AppPage component */}
         <Route path="/" element={<BrakePressCalculator />} />
         {/* Add more routes here as you add more pages */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
